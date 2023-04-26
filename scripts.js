f8da987d8156213860c722d33f332db1fa939ade
Miguel <a22102861@alunos.ulht.pt>
2023-04-26 19:02:25 +0100

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,51 @@
 
 /* Diferentes Disiplinas */
 
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const body = document.querySelector('body');
+
+const butaoDarkMode = document.getElementById('butaoDarkMode');
+butaoDarkMode.onclick = function () {
+    butaoDarkMode.classList.toggle('active');
+    body.classList.toggle('active');
+
+
+
+}
+
+
+/**
+ * 
+ * @param {Date} date 
+ */
+function formatTime(date) {
+
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const isAm = date.getHours() < 12;
+
+    return `${hours12.toString().padStart(2, "0")}:${minutes
+        .toString()
+        .padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
+
+function formatDate(date) {
+    const DIAS = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabádo"];
+    const MESES = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+    return `${DIAS[date.getDay()]}, ${MESES[date.getMonth()]
+        } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+
+
+setInterval(() => {
+    const now = new Date();
+    timeElement.innerHTML = formatTime(now);
+    dateElement.innerHTML = formatDate(now);
+}, 200);
+
 const classes = [
     {
         number: 1,
@@ -127,20 +172,3 @@ courseList.addEventListener('click', function (e) {
         }
     }
 });
-
-// Dark Mode Butão
-
-const darkModeToggle = document.querySelector('.dark-mode-toggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('click', () => {
-    const isDarkMode = body.classList.toggle('dark-mode');
-    darkModeToggle.setAttribute('data-dark-mode', isDarkMode);
-
-    if (isDarkMode) {
-        darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
-    } else {
-        darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-    }
-});
-
